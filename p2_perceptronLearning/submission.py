65367004eb5b76f99b22903acdc3fdcb90e18a49
Meimei <maggie10135@gmail.com>
2023-05-28 19:43:54 +0800

    Finished: voting-classification

--- p2_perceptronLearning/submission.py
+++ p2_perceptronLearning/submission.py
@@ -1,11 +1,11 @@
-import json
-import numpy as np
-import os
-import pandas as pd
 from sklearn import metrics
-from sklearn import preprocessing
-from sklearn.linear_model import Perceptron
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.svm import SVC
 import sys
 import graderUtil
 
@@ -28,12 +28,33 @@ y_test = graderUtil.load_answer_file(answer_filename)
 #print(df.sample(5))
 #print(X_test.sample(5))
 #print(y_test)
-#
+
 ##########################################################
 # BEGIN_YOUR_CODE
 
+## Training ##
+# data spliting
+X = df.iloc[:,[2,3]] 
+y = df.iloc[:,-1]
+# z-score normalization
+scaler = StandardScaler().fit(X) 
+X_scaled = scaler.transform(X)
+X_test_scaled = scaler.transform(X_test.iloc[:,[2,3]])
+X_train, X_validation, y_train, y_validation = train_test_split(X_scaled,y, test_size=0.2, random_state=10)
 
-task_result["y_pred"] = list(np.random.randint(2, size=y_test.shape[0]))
+## Learning ##
+# initialization
+Per_clf = Perceptron()
+LogReg_clf = LogisticRegression()
+DTree_clf = DecisionTreeClassifier()
+SVC_clf = SVC()
+voting_clf = VotingClassifier(estimators=[('Per', Per_clf), ('SVC', SVC_clf), ('DTree', DTree_clf), ('LogReg', LogReg_clf)], voting='hard')
+# optimization
+voting_clf.fit(X_train, y_train)
+# prediction
+y_pred_test = voting_clf.predict(X_test_scaled)
+print(metrics.accuracy_score(y_pred_test, y_test))
+task_result["y_pred"] = y_pred_test;
 
 # END_YOUR_CODE
 # Do Not Make Any Change BELOW
